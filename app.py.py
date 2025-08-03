0baf3d95160ce9f68bbf74cc139780d03543ab1e
nickthies19-png <nickthies19@gmail.com>
2025-08-03 16:56:20 -0400

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -130,15 +130,12 @@ try:
     # ----------------------------
     # DISPLAY RESULTS
     # ----------------------------
-col1, col2 = st.columns(2)
-    
-with col1:
+
     st.subheader("Underlying Info")
     st.write(f"**{ticker_symbol} Current Value:** {S:,.2f}")
     st.write(f"**Strategy Expiry:** {expiration_date} ({days_to_expiration} DTE)")
     st.write("---")
 
-with col2:
     st.subheader("Strategy Results")
     if strategy == "Iron Condor":
         st.write(f"**Suggested Short Call Strike:** {call_strike}")
@@ -147,26 +144,18 @@ with col2:
         st.write(f"**Probability of Touch (Put):** {put_pot:.1%}")
         st.write(f"**Probability Neither Strike Touches:** {prob_neither_touch:.1%}")
         st.write("---")
-            
-        if agree:
-        # Create 2 columns
-        col1, col2 = st.columns(2)
-            #short call stats in left column
-            with col1:
-                st.subheader("Short Call Stats")
-                st.write(f"**IV:** {call_iv:.2%}")
-                st.write(f"**Volume:** {call_volume}")
-                st.write(f"**Open Interest:** {call_oi}")
-                st.write(f"**Bid:** {call_bid}")
-                st.write(f"**Ask:** {call_ask}")
-             #short put stats in right column
-            with col2:
-                st.subheader("Short Put Stats")
-                st.write(f"**IV:** {put_iv:.2%}")
-                st.write(f"**Volume:** {put_volume}")
-                st.write(f"**Open Interest:** {put_oi}")
-                st.write(f"**Bid:** {put_bid}")
-                st.write(f"**Ask:** {put_ask}")
+        st.subheader("Short Call Stats")
+        st.write(f"**IV:** {call_iv:.2%}")
+        st.write(f"**Volume:** {call_volume}")
+        st.write(f"**Open Interest:** {call_oi}")
+        st.write(f"**Bid:** {call_bid}")
+        st.write(f"**Ask:** {call_ask}")
+        st.subheader("Short Put Stats")
+        st.write(f"**IV:** {put_iv:.2%}")
+        st.write(f"**Volume:** {put_volume}")
+        st.write(f"**Open Interest:** {put_oi}")
+        st.write(f"**Bid:** {put_bid}")
+        st.write(f"**Ask:** {put_ask}")
 
         elif strategy == "Short Put":
             st.write(f"**Suggested Short Put Strike:** {put_strike}")
